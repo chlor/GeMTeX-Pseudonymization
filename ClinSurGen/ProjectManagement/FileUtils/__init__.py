58d637b66f0c248d0a0b9b6998f77cfbe2d4ead9
chlor <christina.lohr@uni-jena.de>
2025-01-31 14:29:43 +0100

    Update architecture.
    ** Note: fixed error on running projects
    * New Update on request on projects

--- ClinSurGen/ProjectManagement/FileUtils/__init__.py
+++ ClinSurGen/ProjectManagement/FileUtils/__init__.py
@@ -9,6 +9,20 @@ from collections import defaultdict
 import cassis
 
 
+def create_project_folders(out_directory, out_directory_zip_export, out_directory_surrogate):
+
+    if not os.path.exists(path=out_directory):
+        os.makedirs(name=out_directory)
+
+    if not os.path.exists(path=out_directory_zip_export):
+        os.makedirs(name=out_directory_zip_export)
+
+    #out_directory_surrogate = out_directory + os.sep + 'surrogate'
+    if not os.path.exists(path=out_directory_surrogate):
+        os.makedirs(name=out_directory_surrogate)
+
+
+
 def translate_tag(tag, translation_path=None):
     """
     Translate the given tag to a human-readable format.
