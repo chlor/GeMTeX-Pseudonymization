8305d19f77415108559ebc8533fa34f55a722133
chlor <christina.lohr@uni-jena.de>
2025-05-15 20:00:04 +0200

    Update fictive modus together with gemtex modus.

---
+++ const.py
@@ -0,0 +1,5 @@
+HOSPITAL_DATA_PATH = './resources/Location_Lists/Combined_healthcare_facilities.txt'
+HOSPITAL_NEAREST_NEIGHBORS_MODEL_PATH = './resources/model/nearest_neighbors_model.joblib'
+
+EMBEDDING_MODEL_NAME = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
+SPACY_MODEL = 'de_core_news_lg'
