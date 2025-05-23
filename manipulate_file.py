b60e392e6cf869f44725b493951c179c92a8dfa8
chlor <christina.lohr@uni-jena.de>
2025-05-23 11:46:35 +0200

    Update manipulate_file.py

--- manipulate_file.py
+++ manipulate_file.py
@@ -1,71 +1,24 @@
-import json
-import os
-import random
 from cassis import *
-from datetime import timedelta
 from GeMTeXSurrogator.Substitution.CASmanagement import manipulate_cas
-from GeMTeXSurrogator.ProjectManagement.FileUtils import export_cas_to_file
 
 
-print('Manipulate 1 File.')
-
-delta = timedelta(random.randint(-365, 365))
-with open('test_data/TypeSystem.xml', 'rb') as f:
+with open('resources/excepted_layers/GeMTeX/TypeSystem.xml', 'rb') as f:
     typesystem = load_typesystem(f)
 
-f_name = 'test_data/annotation_Bastrup.xmi'
 
-print('xmi file', f_name)
+def manipulate_single_cas(f_name, mode):
 
-#for mode in ['X']:
-#for mode in ['entity']:
-#for mode in ['MIMIC_ext']:
-#for mode in ['real_names']
-
-modes = ['real_names']
-
-for mode in modes:
-    print('mode', mode)
+    print('xmi file', f_name)
 
     with open(f_name, 'rb') as f:
-        test_cas = load_cas_from_xmi(f, typesystem=typesystem)
+        orig_cas = load_cas_from_xmi(f, typesystem=typesystem)
 
-    if mode == 'inter_format':
-        m_cas, rand_keys = manipulate_cas(cas=test_cas, delta=timedelta(random.randint(-365, 365)), mode=mode)
-        with open('test_data' + os.sep + 'key_assignment.json', "w") as outfile:
-            json.dump(obj=rand_keys, fp=outfile, indent=2, sort_keys=False)
-    else:
-        m_cas = manipulate_cas(
-            cas=test_cas,
-            delta=timedelta(random.randint(-365, 365)),  # todo
-            mode=mode
-        )
+    return_values = manipulate_cas(cas=orig_cas, mode=mode, used_keys=[])
 
-    export_cas_to_file(
-        cas=m_cas,
-        mode=mode,
-        file_name_dir='test_data/',
-        file_name='Baastrup.txt'
-    )
+    print(return_values)
+    print(return_values['cas'].get_sofa())
+    print(return_values['key_ass'])
+    print(return_values['used_keys'])
 
 
-#  Läuft, mit Warnung Not mapping internal offset [9977] which is not valid within the external range [0-9973]
-
-#cas2 = manipulate_cas(cas=cas, delta=delta, mode='entity')
-#export_file(cas=cas1, mode='entity', file_name_dir='test_data/', file_name='Albers.txt')
-
-# Läuft mit mehreren Warnungen, Offset passt nicht mehr
-
-#cas3 = manipulate_cas(cas=cas, delta=delta, mode='MIMIC_ext')
-#export_file(cas=cas1, mode='MIMIC_ext', file_name_dir='test_data/', file_name='Albers.txt')
-
-# Läuft mit einem Fehler
-
-#cas4 = manipulate_cas(cas=cas, delta=delta, mode='real_names')
-#export_file(cas=cas1, mode='real_names', file_name_dir='test_data/', file_name='Albers.txt')
-
-
-
-
-
-
+manipulate_single_cas(f_name='test_data/XMI-JSON/grascco_phi_pii_2/Boeck.txtphi-pii_2.0.xmi', mode='fictive')
